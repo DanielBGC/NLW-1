551b23ecee6aab518e8ff4190b5b98fe8afeaff2
Daniel Borba Guimarães da Costa <daniel.borba-@hotmail.com>
2020-06-03 13:40:13 -0300

    Upload das pastas contendo os scripts e os estilos utilizados

---
+++ scripts/register.js
@@ -0,0 +1,57 @@
+const ufSelect = document.querySelector("select[name=uf]")
+const citySelect = document.querySelector("select[name=city]")
+
+function getStates() {
+    const stateApi = fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados") 
+
+    //THEN = caminho de sucesso
+    stateApi.then(function(response) {
+        return(response.json())
+    }).then(function(states) {
+        for(let state of states) {
+            ufSelect.innerHTML += `<option value="${state.id}"> ${state.nome} </option>`
+        }
+    })
+
+    //CATCH = caminho de erro
+    stateApi.catch(function() {
+        console.warn("ERROR 404")
+    })
+}
+getStates()
+
+function getCities(event) {
+    const stateInput = document.querySelector("input[name=state]")
+
+    const indexOfSelectedState = event.target.selectedIndex;
+
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    
+    let ufId = event.target.value
+    
+    const cityApi = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufId}/municipios`
+
+    fetch(cityApi)
+    .then(function(response) {
+        return(response.json())
+    }).then(function(cities) {
+        for(let city of cities) {
+            citySelect.innerHTML += `<option value="${city.id}"> ${city.nome} </option>`
+        }
+
+        citySelect.disabled = false;
+    })
+}
+
+function getCityName(event) {
+    const cityInput = document.querySelector("input[name=city]")
+
+    const indexOfSelectedCity = event.target.selectedIndex;
+
+    cityInput.value = event.target.options[indexOfSelectedCity].text
+}
+
+ufSelect.addEventListener("change", getCities)
+
+citySelect.addEventListener("change", getCityName)
