292e7b93d5ea4ac8f9d256125252d3bd207a4302
Daniel Borba <daniel.borba-@hotmail.com>
2020-06-04 18:42:01 -0300

    Instalando e configurando o nunjucks
    Nunjucks utilizado para fazer a comunicação entre o HTML e o back-end

--- src/server.js
+++ src/server.js
@@ -6,20 +6,28 @@ const server = express()
 //configura a pasta public
 server.use(express.static("public"))
 
+//configurando a template engine (comunicação entre o HTML e o back-end)
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //configura caminhos do servidor:
 //(1) página inicial
 server.get("/",function(require, response) {
-    response.sendFile(__dirname + "/views/index.html")
+    //2º parâmetro = variável a ser enviada para o HTML pelo nunjucks
+    return response.render("index.html")
 })
 
 //(2) página de cadastro
 server.get("/register",function(require, response) {
-    response.sendFile(__dirname + "/views/register.html")
+    return response.render("register.html")
 })
 
 //(3) página de listagem
 server.get("/search",function(require, response) {
-    response.sendFile(__dirname + "/views/search.html")
+    return response.render("search.html")
 })
 
 //liga o servidor na porta 3000
